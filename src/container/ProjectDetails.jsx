a0712e66b2e90509622fe4913fce79478297df60
Rana Aldosari <rana@example.com>
2025-06-12 01:02:30 +0300

    update

--- src/container/ProjectDetails.jsx
+++ src/container/ProjectDetails.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-
+import Swal from 'sweetalert2';
 function ProjectDetails() {
   const { id } = useParams();
   const apiUrl = "https://68219a91259dad2655afc3cc.mockapi.io/api/users/image";
@@ -17,6 +17,26 @@ function ProjectDetails() {
         console.log(err);
       });
   }, [id]);
+function acceptproject(){
+ Swal.fire('Project Accepted!');
+}
+ function rejectproject() {
+    Swal.fire({
+      title: 'Why do you reject this project?',
+      input: 'text',
+      inputPlaceholder: 'Type your reason here...',
+      showCancelButton: true,
+      confirmButtonText: 'Send',
+      cancelButtonText: 'Cancel',
+}).then((result) => {
+      if (result.isConfirmed) {
+        const reason = result.value;
+        console.log("Rejected with reason:", reason);
+        Swal.fire('Project Rejected!', `Reason: ${reason}`, 'info');
+      }
+    });
+  }
+
 
   return (
     <>
@@ -30,6 +50,10 @@ function ProjectDetails() {
           <div key={item.id} className='border p-4 mb-4'>
             <h3 className='text-lg font-semibold'>{item.title}</h3>
             <p>{item.description}</p>
+<div className='flex gap-3'>
+                <button className='bg-green-400' onClick={acceptproject}>Accept</button>
+            <button onClick={rejectproject} className='bg-red-500'>ignor</button>
+</div>
           </div>
         ))
       )}
